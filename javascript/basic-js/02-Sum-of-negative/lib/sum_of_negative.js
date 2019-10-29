b0864045b5d394f015f130b2bd835bb3e87608f6
Zhihao Cao <caozhihaorex@hotmail.com>
2019-10-29 22:35:29 +0800

    Looping

--- javascript/basic-js/02-Sum-of-negative/lib/sum_of_negative.js
+++ javascript/basic-js/02-Sum-of-negative/lib/sum_of_negative.js
@@ -1,3 +1,16 @@
 sumOfNegative = (numbers) => {
-  // TODO: You are getting a `numbers` array. Return the sum of **negative** numbers only.
+  // TDO: You are getting a `numbers` array. Return the sum of **negative** numbers only.
+  let sum = 0;
+  numbers.forEach((number) => {
+    if (number < 0) {
+      sum = sum + number;
+    }
+  })
+
+  return sum
 }
+
+// scan and pick out numbers that are negative, add them up, and return the result//
+
+sumOfNegative([-1, 4, -2, 9, 18]) == -3
+
