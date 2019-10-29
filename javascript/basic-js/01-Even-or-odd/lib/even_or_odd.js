2595cf7801bea137e1eb2973612e16ee72c79f00
Zhihao Cao <caozhihaorex@hotmail.com>
2019-10-29 21:46:06 +0800

    evenorodd?

---
+++ javascript/basic-js/01-Even-or-odd/lib/even_or_odd.js
@@ -0,0 +1,16 @@
+evenOrOdd = (number) => {
+
+  if (number % 2 == 1) {
+    return "odd";
+  } else {
+    return "even";
+  }
+   // TODO: this should return "even" if the number is even, "odd" otherwise
+}
+
+// if number / 2 equals an integer than show "even"
+// else than show "odd"
+
+
+
+
