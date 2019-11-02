97d59579f680de58204fa1680efb9827d8630a16
Zhihao Cao <caozhihaorex@hotmail.com>
2019-11-02 11:58:34 +0800

    First-Listener

--- javascript/dom-events/02-My-First-Event-Listener/lib/listener.js
+++ javascript/dom-events/02-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,11 @@
 // TODO: React to a click on the button!
+const button = document.querySelector("#clickme")
+const music = document.querySelector("audio")
+button.classList.add ("disabled")
+button.addEventListener("click", (event) => {
+  button.classList.add("disabled")
+  button.innerText = "Bingo"
+  music.play()
+})
+
+
